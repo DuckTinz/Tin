fa6ce53fa19e714c2c78e7f2f969a53837d12aff
DuckTinz <tinpdgcs210655@fpt.edu.vn>
2023-06-26 19:57:58 +0700

    first commit

--- server.js
+++ server.js
@@ -1,75 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
 const connectionString = 'mongodb+srv://tinpdgcs210655:abcd1234@cluster0.xvafcai.mongodb.net/';
 
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to Database');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-    const productSchema = new mongoose.Schema({
-      name: String,
-      price: Number,
-      image: String,
-      description: String
-    });
-
-    const Product = mongoose.model('Product', productSchema);
-
-    app.set('view engine', 'ejs');
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(express.static('public'));
-    app.use(bodyParser.json());
-
-    app.post('/products', (req, res) => {
-      const { name, price, image, description } = req.body;
-
-      const newProduct = new Product({
-        name,
-        price,
-        image,
-        description
-      });
-
-      newProduct.save()
-        .then(() => {
-          res.redirect('/products');
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(500).send('Error adding product');
-        });
-    });
-    app.get('/', function(req, res) {
-        Product.find()
-          .then(products => {
-            res.render('index', { products });
-          })
-          .catch(err => {
-            console.error(err);
-            res.status(500).send('Error fetching products');
-          });
-      });
-    app.get('/products/add', (req, res) => {
-        res.render('add-product');
-      });
-    app.get('/products', (req, res) => {
-      Product.find()
-        .then(products => {
-          res.render('products', { products });
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(500).send('Error fetching products');
-        });
-    });
-
-    app.listen(3000, function () {
-      console.log('Listening on 3000');
-    });
-  })
-  .catch(err => {
+MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
     console.error('Error connecting to database:', err);
+    return;
+  }
+
+  const db = client.db('<database>');
+  const collection = db.collection('products');
+
+  app.post('/products', (req, res) => {
+    const { name, price, image, description } = req.body;
+
+    const newProduct = {
+      name,
+      price,
+      image,
+      description
+    };
+
+    collection.insertOne(newProduct)
+      .then(() => {
+        res.redirect('/products');
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Error adding product');
+      });
   });
+
+  app.get('/', (req, res) => {
+    collection.find().toArray()
+      .then(products => {
+        res.render('index', { products });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Error fetching products');
+      });
+  });
+
+  app.get('/products/add', (req, res) => {
+    res.render('add-product');
+  });
+
+  app.get('/products', (req, res) => {
+    collection.find().toArray()
+      .then(products => {
+        res.render('products', { products });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Error fetching products');
+      });
+  });
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+});
